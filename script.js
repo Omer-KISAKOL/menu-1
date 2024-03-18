0abe4cb25ad54a4ad49afebb21a03cbb8593e027
omer- <okisakol@gmail.com>
2024-03-19 01:45:30 +0300

    Initial commit

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ container.addEventListener('mousedown', (e) => {
     isDown = true;
     startX = e.pageX - container.offsetLeft;
     scrollLeft = container.scrollLeft;
+    container.style.cursor = 'grabbing';
 });
 
 container.addEventListener('mouseleave', () => {
     isDown = false;
+    container.style.cursor = 'grab';
 });
 
 container.addEventListener('mouseup', () => {
@@ -22,9 +24,26 @@ container.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     e.preventDefault();
     const x = e.pageX - container.offsetLeft;
-    const walk = (x - startX) * 10; // Scroll hızı
+    const walk = (x - startX) * 3; // Scroll hızı
     container.scrollLeft = scrollLeft - walk;
-    container.style.cursor = 'grabbing';
+});
+
+container.addEventListener('touchstart', (e) => {
+    isDown = true;
+    startX = e.touches[0].pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('touchend', () => {
+    isDown = false;
+});
+
+container.addEventListener('touchmove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - container.offsetLeft;
+    const walk = (x - startX) * 3; // Scroll hızı
+    container.scrollLeft = scrollLeft - walk;
 });
 container.addEventListener('transitionend', () => {
     container.style.cursor = 'grab';
